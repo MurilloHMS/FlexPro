e178685f8a9283c628029c1271a0f3e4570bc156
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-11-13 12:46:02 -0300

    melhorado cadastro e criado grid para ver produtos

--- FlexPro/Data/ApplicationDbContext.cs
+++ FlexPro/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using FlexPro.Models;
 
 namespace FlexPro.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
 {
+
+public DbSet<FlexPro.Models.Veiculo> Veiculo { get; set; } = default!;
 }
