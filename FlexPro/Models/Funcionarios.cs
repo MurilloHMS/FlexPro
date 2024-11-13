5f206f4b61440b74cda521a1f81d7d2ce8b4fda4
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-11-13 10:32:54 -0300

    incluido modelo das classes

---
+++ FlexPro/Models/Funcionarios.cs
@@ -0,0 +1,13 @@
+﻿namespace FlexPro.Models
+{
+    public class Funcionarios
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public string? Departamento { get; set; }
+        public string? Hash { get; set; }
+        public string? Gerente { get; set; }
+        public string? Email { get; set; }
+    }
+
+}
---
+++ FlexPro/Models/ICMS.cs
@@ -0,0 +1,7 @@
+namespace FlexPro.Models;
+
+public class ICMS
+{
+    public decimal vICMS { get; set; }
+    public string  nNF { get; set; }
+}
