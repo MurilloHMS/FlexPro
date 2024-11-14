ae02d3e0df4c3da0453831a55334a32058a893c2
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-11-14 11:33:35 -0300

    incluido contexto do abastecimento

--- FlexPro/Data/ApplicationDbContext.cs
+++ FlexPro/Data/ApplicationDbContext.cs
@@ -9,4 +9,5 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
 
 public DbSet<FlexPro.Models.Veiculo> Veiculo { get; set; } = default!;
+public DbSet<Abastecimento> Abastecimento { get; set; } = default!;
 }
