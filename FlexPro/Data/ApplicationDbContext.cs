39f731893df5f8afe8eb5240dc16231fb60eaac3
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-11-18 07:51:30 -0300

    gerado entidade categorias

--- FlexPro/Data/ApplicationDbContext.cs
+++ FlexPro/Data/ApplicationDbContext.cs
@@ -11,4 +11,5 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 public DbSet<FlexPro.Models.Veiculo> Veiculo { get; set; } = default!;
 public DbSet<Abastecimento> Abastecimento { get; set; } = default!;
 public DbSet<Funcionarios> Funcionarios { get; set; } = default!;
+public DbSet<Categoria> Categoria { get; set; } = default!;
 }
--- FlexPro/Data/Migrations/20241118105042_Categorias.Designer.cs
+++ FlexPro/Data/Migrations/20241118105042_Categorias.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FlexPro.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FlexPro.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241118105042_Categorias")]
+    partial class Categorias
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
