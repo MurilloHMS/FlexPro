d729f666178bd674a281f606d1260f24ccd7a49e
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-11-18 08:20:02 -0300

    incluido migrations
    inclui as entidades Receitas, Entidades, Produtos e Revisão

--- FlexPro/Data/ApplicationDbContext.cs
+++ FlexPro/Data/ApplicationDbContext.cs
@@ -6,10 +6,34 @@ namespace FlexPro.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
-{
+    {
+        public DbSet<FlexPro.Models.Veiculo> Veiculo { get; set; } = default!;
+        public DbSet<Abastecimento> Abastecimento { get; set; } = default!;
+        public DbSet<Funcionarios> Funcionarios { get; set; } = default!;
+        public DbSet<Categoria> Categoria { get; set; } = default!;
+        public DbSet<Entidade> Entidade { get; set; } = default!;
+        public DbSet<Receita> Receita { get; set; } = default!;
+        public DbSet<Revisao> Revisao { get; set; } = default!;
+        public DbSet<Produto> Produto { get; set; } = default!;
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<Revisao>()
+                .HasOne(r => r.Local)
+                .WithMany()
+                .HasForeignKey(r => r.LocalId);
 
-public DbSet<FlexPro.Models.Veiculo> Veiculo { get; set; } = default!;
-public DbSet<Abastecimento> Abastecimento { get; set; } = default!;
-public DbSet<Funcionarios> Funcionarios { get; set; } = default!;
-public DbSet<Categoria> Categoria { get; set; } = default!;
-}
+            modelBuilder.Entity<Revisao>()
+                .HasOne(r => r.Veiculo)
+                .WithMany()
+                .HasForeignKey(r => r.VeiculoId);
+
+            modelBuilder.Entity<Produto>()
+                .HasOne(p => p.Receita)
+                .WithMany()
+                .HasForeignKey(p => p.IdReceita);
+        }
+    }
+    
