17681e460e66edadc5b2ef6bd653bce6de7d062b
MurilloHMS <murillohenriquemelo@outlook.com>
2024-11-21 13:02:55 -0300

    iniciado cadastro de abastecimentos

--- FlexPro/Models/SepararPDF.cs
+++ FlexPro/Models/SepararPDF.cs
@@ -29,25 +29,17 @@ namespace FlexPro.Models
         {
             if (!File.Exists(inputPdfPath)) return;
 
-            // Open the document with Import mode
             using (var inputDocument = PdfReader.Open(inputPdfPath, PdfDocumentOpenMode.Import))
             {
                 for (int pageNumber = 0; pageNumber < inputDocument.PageCount; pageNumber++)
                 {
-                    // Create a new document for each page
                     using (var outputDocument = new PdfDocument())
                     {
-                        // Import the page
                         var page = inputDocument.Pages[pageNumber];
-                        // Create a new page in the output document with the same size and properties
                         var newPage = outputDocument.AddPage(page);
-
-                        // Optionally, you can copy other attributes from the page here if necessary
-
-                        // Define the output file path
+                        
                         string outputFilePath = Path.Combine(outputFolder, $"{lista[pageNumber].Nome}.pdf");
 
-                        // Save the output document
                         outputDocument.Save(outputFilePath);
                     }
                 }
