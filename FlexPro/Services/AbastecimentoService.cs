f0993b0cb8e7037184fd140e2e46edafc958bc25
MurilloHMS <murillohenriquemelo@outlook.com>
2024-12-09 15:51:52 -0300

    melhorado código do abastecimento

--- FlexPro/Services/AbastecimentoService.cs
+++ FlexPro/Services/AbastecimentoService.cs
@@ -27,31 +27,10 @@ public class AbastecimentoService
 
         var sb = new StringBuilder();
 
-        var totalLitrosMesAtual = abastecimentoMesAtual.Sum(a => a.Litros);
-        var totalLitrosMesAnterior = abastecimentoMesAnterior.Sum(a => a.Litros);
-
-        var totalPercorridoMesAtual = abastecimentoMesAtual.Sum(a => a.DiferencaHodometro);
-        var totalPercorridoMesAnterior = abastecimentoMesAnterior.Sum(a => a.DiferencaHodometro);
-        
-        var mediaKmMesAtual = abastecimentoMesAtual.Average(a => a.MediaKm);
-        var mediaKmMesAnterior = abastecimentoMesAnterior.Average(a => a.MediaKm);
-
-        var valorTotalGastoMesAtual = abastecimentoMesAtual.Sum(a => a.ValorTotalTransacao);
-        var valorTotalGastoMesAnterior = abastecimentoMesAnterior.Sum(a => a.ValorTotalTransacao);
-
-        var mediaPrecoLitroMesAtual = abastecimentoMesAtual.Average(a => a.Preco);
-        var mediaPrecoLitroMesAnterior = abastecimentoMesAnterior.Average(a => a.Preco);
-        
-        sb.AppendLine($"Abastecimento Geral");
-        sb.AppendLine($"Quantidade de litros abastecidos: {Calculos.CalcularDesempenho(totalLitrosMesAtual, totalLitrosMesAnterior,"N")}");
-        sb.AppendLine($"Média de KM/L: {Calculos.CalcularDesempenho(mediaKmMesAtual, mediaKmMesAnterior, "N")}");
-        sb.AppendLine($"Média de Preço/L: {Calculos.CalcularDesempenho(mediaPrecoLitroMesAtual, mediaPrecoLitroMesAnterior, "C")}");
-        sb.AppendLine($"Valor Total Gasto: {Calculos.CalcularDesempenho(valorTotalGastoMesAtual, valorTotalGastoMesAnterior, "C")}");
-        sb.AppendLine($"Distancia percorrida: {Calculos.CalcularDesempenho(totalPercorridoMesAtual, totalPercorridoMesAnterior, "N0")}");
+        sb.AppendLine(await CalculaAbastecimento(abastecimentoMesAtual, abastecimentoMesAnterior, "Geral"));
         
         return sb.ToString();
     }
-    
     public async Task<string> CalcularAbastecimentoSetor(DateTime data)
     {
         var dataAtual = data.ToUniversalTime();
@@ -68,32 +47,10 @@ public class AbastecimentoService
 
         foreach (var departamento in departamentos)
         {
-            var abastecimentoAtualDepto = abastecimentoMesAtual.Where(a => a.Departamento == departamento);
-            var abastecimentoAnteriorDepto = abastecimentoMesAnterior.Where(a => a.Departamento == departamento);
-
-            var totalLitrosMesAtual = abastecimentoAtualDepto.Sum(a => a.Litros);
-            var totalLitrosMesAnterior = abastecimentoAnteriorDepto.Sum(a => a.Litros);
-
-            var totalPercorridoMesAtual = abastecimentoAtualDepto.Sum(a => a.DiferencaHodometro);
-            var totalPercorridoMesAnterior = abastecimentoAnteriorDepto.Sum(a => a.DiferencaHodometro);
+            var abastecimentoAtualDepto = abastecimentoMesAtual.Where(a => a.Departamento == departamento).ToList();
+            var abastecimentoAnteriorDepto = abastecimentoMesAnterior.Where(a => a.Departamento == departamento).ToList();
             
-            var mediaKmMesAtual = abastecimentoAtualDepto.Average(a => a.MediaKm);
-            var mediaKmMesAnterior = abastecimentoAnteriorDepto.Average(a => a.MediaKm);
-
-            var valorTotalGastoMesAtual = abastecimentoAtualDepto.Sum(a => a.ValorTotalTransacao);
-            var valorTotalGastoMesAnterior = abastecimentoAnteriorDepto.Sum(a => a.ValorTotalTransacao);
-
-            var mediaPrecoLitroMesAtual = abastecimentoAtualDepto.Average(a => a.Preco);
-            var mediaPrecoLitroMesAnterior = abastecimentoAnteriorDepto.Average(a => a.Preco);
-
-            sb.AppendLine($"Departamento {departamento}");
-            sb.AppendLine($"Quantidade de litros abastecidos: {Calculos.CalcularDesempenho(totalLitrosMesAtual, totalLitrosMesAnterior,"N")}");
-            sb.AppendLine($"Média de KM/L: {Calculos.CalcularDesempenho(mediaKmMesAtual, mediaKmMesAnterior, "N")}");
-            sb.AppendLine($"Média de Preço/L: {Calculos.CalcularDesempenho(mediaPrecoLitroMesAtual, mediaPrecoLitroMesAnterior, "C")}");
-            sb.AppendLine($"Valor Total Gasto: {Calculos.CalcularDesempenho(valorTotalGastoMesAtual, valorTotalGastoMesAnterior, "C")}");
-            sb.AppendLine($"Distancia percorrida: {Calculos.CalcularDesempenho(totalPercorridoMesAtual, totalPercorridoMesAnterior, "N0")}");
-            sb.AppendLine();
-
+            sb.AppendLine(await CalculaAbastecimento(abastecimentoAtualDepto, abastecimentoAnteriorDepto, $"{departamento}"));
         }
         return sb.ToString();
     }
@@ -112,36 +69,14 @@ public class AbastecimentoService
 
         var sb = new StringBuilder();
 
-        foreach (var funcionario in funcionarios)
+        foreach (var funcionario in funcionarios.OrderBy(a => a))
         {
             try
             {
-                var abastecimentoAtualFuncionario = abastecimentoMesAtual.Where(a => a.NomeDoMotorista == funcionario);
-                var abastecimentoAnteriorFuncionario =
-                    abastecimentoMesAnterior.Where(a => a.NomeDoMotorista == funcionario);
+                var abastecimentoAtualFuncionario = abastecimentoMesAtual.Where(a => a.NomeDoMotorista == funcionario).ToList();
+                var abastecimentoAnteriorFuncionario = abastecimentoMesAnterior.Where(a => a.NomeDoMotorista == funcionario).ToList();
 
-                var totalLitrosMesAtual = abastecimentoAtualFuncionario.Sum(a => a.Litros);
-                var totalLitrosMesAnterior = abastecimentoAnteriorFuncionario.Sum(a => a.Litros);
-
-                var totalPercorridoMesAtual = abastecimentoAtualFuncionario.Sum(a => a.DiferencaHodometro);
-                var totalPercorridoMesAnterior = abastecimentoAnteriorFuncionario.Sum(a => a.DiferencaHodometro);
-
-                var mediaKmMesAtual = abastecimentoAtualFuncionario.Average(a => a.MediaKm);
-                var mediaKmMesAnterior = abastecimentoAnteriorFuncionario.Average(a => a.MediaKm);
-
-                var valorTotalGastoMesAtual = abastecimentoAtualFuncionario.Sum(a => a.ValorTotalTransacao);
-                var valorTotalGastoMesAnterior = abastecimentoAnteriorFuncionario.Sum(a => a.ValorTotalTransacao);
-
-                var mediaPrecoLitroMesAtual = abastecimentoAtualFuncionario.Average(a => a.Preco);
-                var mediaPrecoLitroMesAnterior = abastecimentoAnteriorFuncionario.Average(a => a.Preco);
-
-                sb.AppendLine($"Funcionário {funcionario}");
-                sb.AppendLine($"Quantidade de litros abastecidos: {Calculos.CalcularDesempenho(totalLitrosMesAtual, totalLitrosMesAnterior, "N")}");
-                sb.AppendLine($"Média de KM/L: {Calculos.CalcularDesempenho(mediaKmMesAtual, mediaKmMesAnterior, "N")}");
-                sb.AppendLine($"Média de Preço/L: {Calculos.CalcularDesempenho(mediaPrecoLitroMesAtual, mediaPrecoLitroMesAnterior, "C")}");
-                sb.AppendLine($"Valor Total Gasto: {Calculos.CalcularDesempenho(valorTotalGastoMesAtual, valorTotalGastoMesAnterior, "C")}");
-                sb.AppendLine($"Distancia percorrida: {Calculos.CalcularDesempenho(totalPercorridoMesAtual, totalPercorridoMesAnterior, "N0")}");
-                sb.AppendLine();
+                sb.AppendLine(await CalculaAbastecimento(abastecimentoAtualFuncionario, abastecimentoAnteriorFuncionario, $"{funcionario}"));
             }
             catch (Exception)
             {
@@ -149,6 +84,42 @@ public class AbastecimentoService
             }
             
         }
+        return sb.ToString();
+    }
+    
+    private async Task<string> CalculaAbastecimento(List<Abastecimento>? abastecimentoMesAtual, List<Abastecimento>? abastecimentoMesAnterior, string tipo)
+    {
+        var sb = new StringBuilder();
+        try
+        {
+            var totalLitrosMesAtual = abastecimentoMesAtual.Sum(a => a.Litros);
+            var totalLitrosMesAnterior = abastecimentoMesAnterior.Sum(a => a.Litros);
+
+            var totalPercorridoMesAtual = abastecimentoMesAtual.Sum(a => a.DiferencaHodometro);
+            var totalPercorridoMesAnterior = abastecimentoMesAnterior.Sum(a => a.DiferencaHodometro);
+
+            var mediaKmMesAtual = abastecimentoMesAtual.Average(a => a.MediaKm);
+            var mediaKmMesAnterior = abastecimentoMesAnterior.Average(a => a.MediaKm);
+
+            var valorTotalGastoMesAtual = abastecimentoMesAtual.Sum(a => a.ValorTotalTransacao);
+            var valorTotalGastoMesAnterior = abastecimentoMesAnterior.Sum(a => a.ValorTotalTransacao);
+
+            var mediaPrecoLitroMesAtual = abastecimentoMesAtual.Average(a => a.Preco);
+            var mediaPrecoLitroMesAnterior = abastecimentoMesAnterior.Average(a => a.Preco);
+
+            sb.AppendLine($"Abastecimento {tipo}");
+            sb.AppendLine($"Quantidade de litros abastecidos: {Calculos.CalcularDesempenho(totalLitrosMesAtual, totalLitrosMesAnterior, "N")}");
+            sb.AppendLine($"Média de KM/L: {Calculos.CalcularDesempenho(mediaKmMesAtual, mediaKmMesAnterior, "N")}");
+            sb.AppendLine($"Média de Preço/L: {Calculos.CalcularDesempenho(mediaPrecoLitroMesAtual, mediaPrecoLitroMesAnterior, "C")}");
+            sb.AppendLine($"Valor Total Gasto: {Calculos.CalcularDesempenho(valorTotalGastoMesAtual, valorTotalGastoMesAnterior, "C")}");
+            sb.AppendLine($"Distancia percorrida: {Calculos.CalcularDesempenho(totalPercorridoMesAtual, totalPercorridoMesAnterior, "N0")}");
+            sb.AppendLine();
+        }
+        catch (Exception e)
+        {
+            
+        }
+
         return sb.ToString();
     }
 }
