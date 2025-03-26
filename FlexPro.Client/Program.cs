94f5d5cbfc2aa54eba745f653d72c4d9c7fe6888
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-03-26 15:28:23 -0300

    alterado endereço da api

--- FlexPro.Client/Program.cs
+++ FlexPro.Client/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddScoped(sp =>
 {
     var client = new HttpClient
     {
-        BaseAddress = new Uri("http://10.0.4.200:5000")
+        BaseAddress = new Uri("https://10.0.4.200:5001")
     };
     return client;
 });
