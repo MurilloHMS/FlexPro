f43e870297b7ddf32ea2b1c8dae40fae8424c337
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-04-11 15:40:54 -0300

    fix error

--- FlexPro.Client/wwwroot/service-worker.published.js
+++ FlexPro.Client/wwwroot/service-worker.published.js
@@ -9,7 +9,7 @@ const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html$/, /\.js$/,
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
 // Subdiretório base no GitHub Pages
-const base = "https://murillohms.github.io/FlexPro/";
+const base = "/FlexPro/";
 const baseUrl = new URL(base, self.origin);
 const manifestPaths = self.assetsManifest.assets.map(asset => new URL(asset.url, baseUrl).pathname);
 
