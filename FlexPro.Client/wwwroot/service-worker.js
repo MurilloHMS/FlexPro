4eaae2ed10b80024f9c0a6db1769510a1aa3df64
MurilloHMS <murillohenriquemelo@outlook.com>
2025-04-25 08:54:07 -0300

    test: teste de configuração para o githubpages

--- FlexPro.Client/wwwroot/service-worker.js
+++ FlexPro.Client/wwwroot/service-worker.js
@@ -1,4 +1,39 @@
-// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('flexpro-cache').then((cache) => {
+            return cache.addAll([
+                '/', 
+                '/index.html',
+                '/css/app.css',
+                '/_framework/blazor.webassembly.js',
+                '/_content/MudBlazor/MudBlazor.min.css',
+                '/icon-512.png',
+                '/icon-192.png',
+                '/manifest.webmanifest'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['flexpro-cache'];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
